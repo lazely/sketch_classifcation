f1aa466a4adc4736b714cab3b40057c51cdf700c
wonjeongjeong <g1042702@gmail.com>
2024-09-20 23:47:56 +0900

    validation 구분

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -46,49 +46,45 @@ def get_transform(config, is_train=True):
     
 class CustomDataset(Dataset):
     def __init__(
-        self, 
-        root_dir: str, 
-        info_file: str, 
+        self,
+        root_dir: str,
+        info_file: str,
         augmented_dir: str,
         augmented_info_file: str,
         transform: Callable,
+        use_augmented: bool = True,
         is_inference: bool = False
     ):
         self.root_dir = root_dir
         self.augmented_dir = augmented_dir
         self.transform = transform
         self.is_inference = is_inference
-        
+        self.use_augmented = use_augmented
         # Read original data
         self.info_df = pd.read_csv(info_file)
         self.image_paths = self.info_df['image_path'].tolist()
-        
-        # Read augmented data
-        self.augmented_df = pd.read_csv(augmented_info_file)
-        self.augmented_image_paths = self.augmented_df['image_path'].tolist()
-
-        # Combine original and augmented data
-        if self.is_inference:
-            self.all_image_paths = self.image_paths
-        else:
+        if self.use_augmented:
+            # Read augmented data
+            self.augmented_df = pd.read_csv(augmented_info_file)
+            self.augmented_image_paths = self.augmented_df['image_path'].tolist()
             self.all_image_paths = self.image_paths + self.augmented_image_paths
-        
+        else:
+            self.all_image_paths = self.image_paths
         if not self.is_inference:
-            self.targets = self.info_df['target'].tolist() + self.augmented_df['target'].tolist()
-
+            if self.use_augmented:
+                self.targets = self.info_df['target'].tolist() + self.augmented_df['target'].tolist()
+            else:
+                self.targets = self.info_df['target'].tolist()
     def __len__(self) -> int:
         return len(self.all_image_paths)
-
     def __getitem__(self, index: int) -> Union[Tuple[torch.Tensor, int], torch.Tensor]:
-        if index < len(self.image_paths):
-            img_path = os.path.join(self.root_dir, self.all_image_paths[index])
-        else:
+        if self.use_augmented and index >= len(self.image_paths):
             img_path = os.path.join(self.augmented_dir, self.all_image_paths[index])
-        
+        else:
+            img_path = os.path.join(self.root_dir, self.all_image_paths[index])
         image = cv2.imread(img_path, cv2.IMREAD_COLOR)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
         image = self.transform(image)
-
         if self.is_inference:
             return image
         else:
@@ -122,12 +118,24 @@ def get_data_loaders(config, batch_size=None):
         info_file=config['data']['train_info_file'],
         augmented_dir=config['data']['augmented_dir'],
         augmented_info_file=config['data']['augmented_info_file'],
-        transform=get_transform(config)
+        transform=get_transform(config, is_train=True),
+        use_augmented=True
     )
-
     train_size = int((1-config['training']['validation_ratio']) * len(train_dataset))
     val_size = len(train_dataset) - train_size
-    train_dataset, val_dataset = torch.utils.data.random_split(train_dataset, [train_size, val_size])
+    train_dataset, _ = torch.utils.data.random_split(train_dataset, [train_size, val_size])
+    
+    val_dataset = CustomDataset(
+        root_dir=config['data']['train_dir'],
+        info_file=config['data']['train_info_file'],
+        augmented_dir=None,
+        augmented_info_file=None,
+        transform=get_transform(config, is_train=False),
+        use_augmented=False
+    )
+    train_size = int((1-config['training']['validation_ratio']) * len(val_dataset))
+    val_size = len(val_dataset) - train_size
+    _, val_dataset = torch.utils.data.random_split(val_dataset, [train_size, val_size])
 
     train_loader = DataLoader(
         train_dataset,
